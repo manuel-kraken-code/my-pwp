79d9423577b8e7c6980abebbfc86da8f181ccd67
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-04-18 09:13:04 -0600

    my pwp made simple

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>my PWP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<ul class="nav">
+		<li><a href="milestone-1.php">HOME</a></li>
+		<li><a href="about.html">ABOUT ME</a></li>
+		<li><a href="resume.html">RESUME</a></li>
+		<li><a href="contact.html">CONTACT</a></li>
+	</ul>
+
+	<div id="container">
+	<h1>Professional Freelance Websites</h1>
+	<h2>purpose:</h2>
+		<p></p>
+	<h2>audience:</h2>
+		<p></p>
+	<h2>goal:</h2>
+		<p></p>
+	<h2>Persona:</h2>
+	<h1>Dave Robinson</h1>
+		<ul>
+			<li>AGE: 28</li>
+			<li>Dave is looking for a personal website for himself</li>
+		</ul>
+	</div>
+</body>
+
+</html>
