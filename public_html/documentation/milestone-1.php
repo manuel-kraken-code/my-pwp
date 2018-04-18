f0aa0c7ff00a9963481c74b3d58705872357416a
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-04-18 10:45:21 -0600

    my pwp personal freelance website

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -15,18 +15,20 @@
 	</ul>
 
 	<div id="container">
-	<h1>Professional Freelance Websites</h1>
+	<h1>Personal Freelance Websites</h1>
 	<h2>purpose:</h2>
-		<p></p>
+		<p>To build simple sites. Fast and efficient</p>
 	<h2>audience:</h2>
-		<p></p>
+		<p>People who are seeking Personal sites for themselves.</p>
 	<h2>goal:</h2>
-		<p></p>
+		<p>To Give people fast easy sites done well and to preference.</p>
 	<h2>Persona:</h2>
 	<h1>Dave Robinson</h1>
 		<ul>
 			<li>AGE: 28</li>
+			<li>GENDER:MALE</li>
 			<li>Dave is looking for a personal website for himself</li>
+
 		</ul>
 	</div>
 </body>
