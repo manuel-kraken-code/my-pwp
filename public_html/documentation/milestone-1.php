a9f84c6e757af991554c9fd9c8a85c3cdb371dc2
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-04-26 01:25:16 -0600

    updated my pwp site

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE>
-<html>
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>my PWP</title>
@@ -15,24 +15,27 @@
 	</ul>
 
 	<div id="container">
-	<h1 class="text">Personal Freelance Websites</h1>
+	<h1 class="text">Freelance Professional Portfolio</h1>
 		<h2><span class="text subject">Purpose, Audience, Goal:</span></h2>
-	<h2 class="text">purpose:</h2>
-		<p class="text">To build simple sites. Fast and efficient</p>
-	<h2 class="text">audience:</h2>
-		<p class="text">People who are seeking Personal sites for themselves.</p>
-	<h2 class="text">goal:</h2>
-		<p class="text">To give people fast easy sites done well and to preference.</p>
+	<h2 class="text">Purpose:</h2>
+		<p class="text">To build professional portfolio's. Adding to clients users experience Fast and efficient</p>
+	<h2 class="text">Audience:</h2>
+		<p class="text">People who are seeking Personal portfolio to add to there book. And are wanting a professional look to there
+			image online</p>
+	<h2 class="text">Goal:</h2>
+		<p class="text">To offer people a great fast start for a personal professional portfolio site of there own!
+		Offering well done portfolio web page's! This sites goal will be to add professional portfolio Websites easy to
+		read and nice to look at. Giving you a presence online! If its yourself or a store. These Personal sites will be categorized
+		in what you would like. And you can set up a personal schedule. (Based on my available times) </p>
 	<h2 ><span class="text subject">Persona:</span></h2>
-	<h1 class="text">Dave Goodwin</h1>
+	<h1 class="text">David j Lance</h1>
 		<ul>
-			<li class="text">AGE: 31</li>
+			<li class="text">AGE: 34</li>
 			<li class="text">GENDER: MALE</li>
-			<li class="text">ATTITUDE: Dave has been looking for a web site that can comply
-				with his web site preferences .</li>
-			<li class="text">FRUSTRATION: Needs a site that is not ugly. And good to look at. </li>
-			<li class="text">GOALS: Wants a personal site done within 1 Month. </li>
-			<li class="text">USER STORY: Dave is looking for a personal website for himself that is new and fresh.</li>
+			<li class="text">JOB: Retail business </li>
+			<li class="text">Role: selling goods to the public</li>
+			<li class="text">GOALS: I want a professional portfolio site that tells my story and gives people a reason to
+			see my retail business</li>
 		</ul>
 	</div>
 </body>
