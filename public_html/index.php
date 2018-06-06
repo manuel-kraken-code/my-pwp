bbe165d82e7dc11554250f6ba56a0b13d936614c
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-06-06 11:11:07 -0600

    DONE! also changed title to Escobar505

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>my PWP</title>
+		<title>Escobar505</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
@@ -29,8 +29,6 @@
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-		<title>Escobar505</title>
 
 		</head>
 
